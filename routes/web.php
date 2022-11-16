72ca455f39551cbd9e093f1f6881dd44e5266978
cristianmontes143 <crischuychuyz@gmail.com>
2022-11-16 17:18:22 +0800

    HomePages&css

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::resource('/', PagesController::class);
