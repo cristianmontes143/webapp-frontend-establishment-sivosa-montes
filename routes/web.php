6c7c3edc62b36e282b180759eedb134be08459bd
cristianmontes143 <crischuychuyz@gmail.com>
2023-01-16 23:06:51 +0800

    login and dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\PagesController;
 Route::resource('/', PagesController::class);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.appdashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
