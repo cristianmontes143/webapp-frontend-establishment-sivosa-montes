635e448c69bbdacbdfcd94e5ed2011cb9ee46953
cristianmontes143 <crischuychuyz@gmail.com>
2023-01-18 11:28:59 +0800

    preview

--- resources/views/dashboard/appdashboard.blade.php
+++ resources/views/dashboard/appdashboard.blade.php
@@ -175,35 +175,35 @@
                     <tbody>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
+                        <td><a href="#" class="text-primary fw-bold">Cellphone</a></td>
                         <td>$64</td>
                         <td class="fw-bold">124</td>
                         <td>$5,828</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
+                        <td><a href="#" class="text-primary fw-bold">Laptop</a></td>
                         <td>$46</td>
                         <td class="fw-bold">98</td>
                         <td>$4,508</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
+                        <td><a href="#" class="text-primary fw-bold">Printer</a></td>
                         <td>$59</td>
                         <td class="fw-bold">74</td>
                         <td>$4,366</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/product-4.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
+                        <td><a href="#" class="text-primary fw-bold">Tablet</a></td>
                         <td>$32</td>
                         <td class="fw-bold">63</td>
                         <td>$2,016</td>
                       </tr>
                       <tr>
                         <th scope="row"><a href="#"><img src="assets/img/product-5.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
+                        <td><a href="#" class="text-primary fw-bold">Accesories</a></td>
                         <td>$79</td>
                         <td class="fw-bold">41</td>
                         <td>$3,239</td>
